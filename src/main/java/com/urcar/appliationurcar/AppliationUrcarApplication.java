05616e391b80f6dbff1c145d91b9bfd85389dd86
Jack <jack008007z@outlook.es>
2021-11-23 18:38:43 -0500

    'n'

--- src/main/java/com/urcar/appliationurcar/AppliationUrcarApplication.java
+++ src/main/java/com/urcar/appliationurcar/AppliationUrcarApplication.java
@@ -3,7 +3,10 @@ package com.urcar.appliationurcar;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @OpenAPIDefinition
 @SpringBootApplication
@@ -12,6 +15,15 @@ public class AppliationUrcarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppliationUrcarApplication.class, args);
+	}
+	@Bean()
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://urcargreenbackend.netlify.app").allowedMethods("*").allowedHeaders("*");
+			}
+		};
 	}
 
 }
