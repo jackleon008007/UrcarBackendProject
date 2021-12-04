a2f73ed11cecb6d257a86896d3bc16bca7d7fdd1
Jack <jack008007z@outlook.es>
2021-12-04 17:12:45 -0500

    'newVersion'

--- src/main/java/com/urcar/appliationurcar/userAdministration/mapping/MappingConfiguration.java
+++ src/main/java/com/urcar/appliationurcar/userAdministration/mapping/MappingConfiguration.java
@@ -1,6 +1,6 @@
 package com.urcar.appliationurcar.userAdministration.mapping;
 
-import com.urcar.appliationurcar.publications.mapping.PostMapper;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
